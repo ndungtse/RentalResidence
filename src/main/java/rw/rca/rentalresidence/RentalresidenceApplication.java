9e32395ca45e7adc9e379b66890325095adbdfe5
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2023-02-07 22:05:24 +0200

    recovered dockerfile

--- src/main/java/rw/rca/rentalresidence/RentalresidenceApplication.java
+++ src/main/java/rw/rca/rentalresidence/RentalresidenceApplication.java
@@ -3,8 +3,10 @@ package rw.rca.rentalresidence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Profile;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+//@EnableSwagger2
 public class RentalresidenceApplication {
 
     public static void main(String[] args) {
